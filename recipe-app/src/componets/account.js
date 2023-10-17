e50738e635a80c18e2807deebb96d16a1566d34a
unknown <generatorfox@gmail.com>
2023-10-17 17:36:39 +0300

    working on UI

--- recipe-app/src/componets/account.js
+++ recipe-app/src/componets/account.js
@@ -1,7 +1,16 @@
+import "../styles/account.css";
+
 export default function account() {
   return (
-    <div>
-      <div></div>
+    <div className="account-info">
+      <div>
+        <div className="icon"></div>
+        <div>
+          <h3>Hristo Dobrikov</h3>
+          <p>User</p>
+        </div>
+      </div>
+      <div className="recipes-add"></div>
     </div>
   );
 }
--- recipe-app/src/componets/header.js
+++ recipe-app/src/componets/header.js
@@ -2,14 +2,27 @@ import "../styles/header.css";
 
 export default function header() {
   return (
-    <div className="header">
-      <div className="inputs">
-        <input placeholder="What do you want to cook today?" />
-        <button></button>
+    <section className="bck">
+      <div className="header">
+        <div className="inputs">
+          <input placeholder="What do you want to cook today?" />
+          <button></button>
+        </div>
       </div>
-      <section>
+      <div className="recipes-section">
         <h1>Recommended Recipes</h1>
-      </section>
-    </div>
+        <div className="recipes-box">
+          <div className="recipe"></div>
+          <div className="recipe"></div>
+          <div className="recipe"></div>
+        </div>
+        <h1>Trending Recipes</h1>
+        <div className="recipes-box">
+          <div className="recipe"></div>
+          <div className="recipe"></div>
+          <div className="recipe"></div>
+        </div>
+      </div>
+    </section>
   );
 }
